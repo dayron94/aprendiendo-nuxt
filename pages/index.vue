25192081f1614383aff804d4910c5c26132cb9b5
Dayron Carvajal <dayron1794@gmail.com>
2019-03-21 20:26:40 -0500

    Iniciando proyect

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>hola {{ msg}}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>Nombre</th>
+      </tr>
+      <tr v-for="(item, index) in municipio"  :key="index">
+        <td> {{ item }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "dayron carvajal",
+      municipio: ["Mocoa", "Villa", "Pto Asis"]
+    };
+  },
+  methods: {
+
+     guardar(){
+      this.municipio.push(this.msg)
+       this.msg=''
+     }
+  }
+};
+</script>
+
+<style>
+</style>
+
